74c092e290e831052c1df0c53568375109f6fa8c
Ramon Almeida <ramondeallima@gmail.com>
2019-08-08 22:17:18 -0300

    Código refatorado para automatizar a criação de novas <tr's> (pacientes) já com suas devidas classes.

--- js/form.js
+++ js/form.js
@@ -6,29 +6,13 @@ botaoAdicionar.addEventListener('click', function(event){
     var form = document.querySelector('#form-adiciona')
 
     var paciente = ObterPacienteDoFormulario(form)
-
-    var pacienteTR = document.createElement('tr')
     
-    var nomeTD = document.createElement('td')
-    var pesoTD = document.createElement('td')
-    var alturaTD = document.createElement('td')
-    var gorduraTD = document.createElement('td')
-    var imcTd = document.createElement('td')
-
-    nomeTD.textContent = nome;
-    pesoTD.textContent = peso;
-    alturaTD.textContent = altura;
-    gorduraTD.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura)
-
-    pacienteTR.appendChild(nomeTD)
-    pacienteTR.appendChild(pesoTD)
-    pacienteTR.appendChild(alturaTD)
-    pacienteTR.appendChild(gorduraTD)
-    pacienteTR.appendChild(imcTd)
+    var pacienteTR = MontarTr(paciente)
 
     var tabela = document.querySelector('#tabela-pacientes')
     tabela.appendChild(pacienteTR)
+    
+    form.reset()
 })
 
 function ObterPacienteDoFormulario(form){
@@ -42,4 +26,31 @@ function ObterPacienteDoFormulario(form){
     }
 
     return paciente
+}
+
+function MontarTr(paciente){
+    var pacienteTR = document.createElement('tr')
+    pacienteTR.classList.add('paciente')
+
+    var nomeTD = MontarTD(paciente.nome, 'info-nome')
+    var pesoTD = MontarTD(paciente.peso, 'info-peso')
+    var alturaTD = MontarTD(paciente.altura, 'info-altura')
+    var gorduraTD = MontarTD(paciente.gordura, 'info-gordura')
+    var imcTD = MontarTD(paciente.imc, 'info-imc')
+
+    pacienteTR.appendChild(nomeTD)
+    pacienteTR.appendChild(pesoTD)
+    pacienteTR.appendChild(alturaTD)
+    pacienteTR.appendChild(gorduraTD)
+    pacienteTR.appendChild(imcTD)
+
+    return pacienteTR
+}
+
+function MontarTD(dado, classe) {
+    var td = document.createElement('td')
+    td.textContent = dado;
+    td.classList.add(classe)
+
+    return td
 }
